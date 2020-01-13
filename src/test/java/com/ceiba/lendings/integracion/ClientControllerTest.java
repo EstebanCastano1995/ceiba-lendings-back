ccc53097a67ac89ec67cf7027d5351e0df2ef25d
Esteban Mauricio Castaño Oquendo - Ceiba Software <esteban.castano@ceiba.com.co>
2020-01-13 16:57:15 -0500

    Adding tests

--- src/test/java/com/ceiba/lendings/integracion/ClientControllerTest.java
+++ src/test/java/com/ceiba/lendings/integracion/ClientControllerTest.java
@@ -1,10 +1,13 @@
 package com.ceiba.lendings.integracion;
 
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import com.ceiba.lendings.LendingsApplication;
 import com.ceiba.lendings.aplicacion.command.ClientCommand;
 import com.ceiba.lendings.databuilder.ClientCommandTestDataBuilder;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -14,6 +17,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
@@ -42,13 +46,15 @@ public class ClientControllerTest {
     public void createClient() throws Exception {
         ClientCommand comandoCliente = new ClientCommandTestDataBuilder().build();
         mockMvc.perform(post("/client").contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(comandoCliente))).andExpect(status().isOk());
+                .content(objectMapper.writeValueAsString(comandoCliente))).andExpect(status().isOk())
+                .andExpect((ResultMatcher) content().string("true"));
     }
 
     @Test
     public void deleteClient() throws Exception {
         ClientCommand comandoCliente = new ClientCommandTestDataBuilder().build();
         mockMvc.perform(post("/client/delete").contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(comandoCliente))).andExpect(status().isOk());
+                .content(objectMapper.writeValueAsString(comandoCliente))).andExpect(status().isOk())
+                .andExpect((ResultMatcher) content().string("false"));
     }
 }
