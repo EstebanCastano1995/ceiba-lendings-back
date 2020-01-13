1fd4950f17485c3bf0aefa890e4d3cc33ac2f6f6
Esteban Mauricio Castaño Oquendo - Ceiba Software <esteban.castano@ceiba.com.co>
2020-01-13 14:41:18 -0500

    Configure project

--- src/main/java/com/ceiba/lendings/aplicacion/usecases/UseCase.java
+++ src/main/java/com/ceiba/lendings/aplicacion/usecases/UseCase.java
@@ -1,17 +1,15 @@
 package com.ceiba.lendings.aplicacion.usecases;
 
-import com.ceiba.lendings.aplicacion.excepcion.UseCaseException;
-
 /**
  * This define the methods for an Use Case service in the architecture.
- * @param <a> > Define the object that execute method receive to work properly
- * @param <r> > Define the object result after an execution of Use Case.
+ * @param <T>> Define the object that execute method receive to work properly
+ * @param <R>> Define the object result after an execution of Use Case.
  */
-public interface UseCase<a, r> {
+public interface UseCase<T, R> {
     /**
      * Metodo que define los pasos que se ejecutarán en el caso de uso.
      * @return resultado que depende de la implementación del caso de uso
      * @throws Exception
      */
-    public r execute(a input) throws UseCaseException;
+    public T  execute(R input) throws Exception;
 }
