5b53ee8ad48acf061ba3d9a24529bb5d258c9fec
Esteban Mauricio Castaño Oquendo - Ceiba Software <esteban.castano@ceiba.com.co>
2020-01-20 08:17:48 -0500

    Configure test and controllers

--- src/main/java/com/ceiba/lendings/aplicacion/usecases/client/DeleteClientUseCase.java
+++ src/main/java/com/ceiba/lendings/aplicacion/usecases/client/DeleteClientUseCase.java
@@ -1,6 +1,5 @@
 package com.ceiba.lendings.aplicacion.usecases.client;
 
-import com.ceiba.lendings.aplicacion.factory.ClientFactory;
 import com.ceiba.lendings.aplicacion.usecases.UseCaseVoid;
 import com.ceiba.lendings.dominio.servicio.client.DeleteClientService;
 import org.springframework.stereotype.Component;
@@ -8,12 +7,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class DeleteClientUseCase implements UseCaseVoid<Long> {
 
-    private final ClientFactory clientFactory;
-
     private DeleteClientService deleteClientService;
 
-    public DeleteClientUseCase(ClientFactory clientFactory,DeleteClientService deleteClientService) {
-        this.clientFactory = clientFactory;
+    public DeleteClientUseCase(DeleteClientService deleteClientService) {
         this.deleteClientService=deleteClientService;
     }
 
