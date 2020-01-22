182a2bda5f0c3c0f670966b653b0b903605ceb29
Esteban Mauricio Castaño Oquendo - Ceiba Software <esteban.castano@ceiba.com.co>
2020-01-22 07:17:25 -0500

    Correct security hostpot

--- src/main/java/com/ceiba/lendings/LendingsApplication.java
+++ src/main/java/com/ceiba/lendings/LendingsApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LendingsApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(LendingsApplication.class, args);
+		SpringApplication.run(LendingsApplication.class);
 	}
 }
